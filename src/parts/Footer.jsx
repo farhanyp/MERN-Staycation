021760425fb7355df358fcb8cc6580c0f70e3551
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-06-09 09:45:01 +0700

    add responsive footer

--- src/parts/Footer.jsx
+++ src/parts/Footer.jsx
@@ -4,7 +4,6 @@ import useMediaQuery from "../hooks/useMediaQuery";
 const Footer = () => {
     const isDesktop = useMediaQuery("(min-width: 1444px)");
 
-
     return (
         <>
         {
@@ -48,7 +47,47 @@ const Footer = () => {
                         </div>
                     </div>
                 </section>
-            ) : ""
+            ) : (
+                <section className='mt-[100px]'>
+                    <div className='border-t-[1px] border-gray'>
+                        <div className='container text-gray text-base font-normal pt-5 '>
+                            <div className='flex flex-col items-center justify-center'>
+                                <div>
+                                    <h1 className='text-[26px] font-medium leading-[39px] text-secondary pb-[27px] text-center'>Stay<span className=' text-primary'>cation.</span></h1>
+                                    <p className='text-center'>We Kaboom your beauty holiday instanly and memorable</p>
+                                </div>
+                                <div className='flex flex-col items-center justify-center'>
+                                    <h2 className='text-primary font-medium pt-6 pb-3'>For Beginners</h2>
+                                    <ul>
+                                        <li className='text-center'>New Account</li>
+                                        <li className='text-center'>Start Booking a Room</li>
+                                        <li className='text-center'>Payments</li>
+                                    </ul>
+                                </div>
+                                <div className='flex flex-col items-center justify-center'>
+                                    <h2 className='text-primary font-medium pt-6 pb-3'>Explore Us</h2>
+                                    <ul>
+                                        <li className='text-center'>About</li>
+                                        <li className='text-center'>Privacy Policy</li>
+                                        <li className='text-center'>Terms & Conditions</li>
+                                    </ul>
+                                </div>
+                                <div className='flex flex-col items-center justify-center'>
+                                    <h2 className='text-primary font-medium pt-6 pb-3'>Getting Touch</h2>
+                                    <ul>
+                                        <li className='text-center'>support@staycation.id</li>
+                                        <li className='text-center'>021-2208-1996</li>
+                                        <li className='text-center'>Staycation Oy,  Jakarta</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='py-[50px] text-center'>
+                                <p>Copyright 2019 * All rights reserved * Staycation</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            )
             }
         </>
     );
